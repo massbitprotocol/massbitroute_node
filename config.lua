7443f3b509ec72c8a190cbafe026d4ec3b2e5d78
Vu Tran <baysao@gmail.com>
2022-04-05 14:38:26 +0700

    Add task monitor_client & move conflict option to etc

--- config.lua
+++ config.lua
@@ -8,12 +8,14 @@ local _config = {
     },
     templates = {},
     apps = {},
-    supervisor = [[
+    supervisors = {
+        ["monitor_client"] = [[
 [program:monitor_client]
 command=/bin/bash _SITE_ROOT_/etc/mkagent/agents/push.sh _SITE_ROOT_
 autorestart=true
 redirect_stderr=true
 stdout_logfile=_SITE_ROOT_/logs/monitor_client.log
     ]]
+    }
 }
 return _config
