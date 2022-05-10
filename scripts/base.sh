7ac8a0f283314c8648526910b915ab3d9d7fb874
Vu Tran <baysao@gmail.com>
2022-05-10 16:08:28 +0700

    Update replace MBRAPI to PORTAL_URL

--- scripts/base.sh
+++ scripts/base.sh
@@ -41,13 +41,20 @@ _update_sources() {
 	branch=$MBR_ENV
 	for _pathgit in $@; do
 		_path=$(echo $_pathgit | cut -d'|' -f1)
-		tmp="$(timeout 60 git -C $_path pull origin $branch 2>&1)"
-		echo "$tmp"
-		echo "$tmp" | grep -i "error"
-		if [ $? -eq 0 ]; then
-			timeout 60 git -C $_path reset --hard
-			tmp="$(timeout 60 git -C $_path pull origin $branch 2>&1)"
-		fi
+		_url=$(echo $_pathgit | cut -d'|' -f2)
+		_branch=$(echo $_pathgit | cut -d'|' -f3)
+		if [ -z "$_branch" ]; then _branch=$branch; fi
+		git -C $_path fetch --all
+
+		git -C $_path checkout $_branch
+		git -C $_path reset --hard
+		tmp="$(git -C $_path pull 2>&1)"
+		# echo "$tmp"
+		# echo "$tmp" | grep -i "error"
+		# if [ $? -eq 0 ]; then
+		# 	timeout 60 git -C $_path reset --hard
+		# 	tmp="$(timeout 60 git -C $_path pull origin $_branch 2>&1)"
+		# fi
 
 		echo "$tmp" | grep -i "updating"
 		st=$?
