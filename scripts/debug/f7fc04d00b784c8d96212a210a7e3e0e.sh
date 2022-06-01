3e7aed212d85a19989be0ef01822df051a74edb3
Vu Tran <baysao@gmail.com>
2022-06-01 21:37:14 +0700

    Fix machine id f7fc04d00b784c8d96212a210a7e3e0e

--- scripts/debug/f7fc04d00b784c8d96212a210a7e3e0e.sh
+++ scripts/debug/f7fc04d00b784c8d96212a210a7e3e0e.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-supervisorctl stop mbr_node
-supervisorctl stop mbr_gateway
-supervisorctl stop mbr_gateway_monitor
-rm /etc/supervisor/conf.d/gateway.conf
-supervisorctl update
-sleep 1
+# supervisorctl stop mbr_node
+# supervisorctl stop mbr_gateway
+# supervisorctl stop mbr_gateway_monitor
+# rm /etc/supervisor/conf.d/gateway.conf
+# supervisorctl update
+# sleep 1
 pkill -f "python_env/gbc/bin/supervisord"
+pkill nginx
 sleep 5
-supervisorctl start mbr_node
+# supervisorctl start mbr_node
 supervisorctl start all
