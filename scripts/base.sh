9090a9a95d462797ce056076b270f345cb48d5ce
Vu Tran <baysao@gmail.com>
2022-07-11 17:02:45 +0700

    fix: private_repo update at runtime

--- scripts/base.sh
+++ scripts/base.sh
@@ -22,12 +22,16 @@ _git_clone() {
 	mkdir -p $_dir
 	if [ ! -d "$_dir/.git" ]; then
 		git clone $_url $_dir -b $_branch
+		git -C $_dir fetch --all
 		git -C $_dir branch --set-upstream-to=origin/$_branch
-	fi
-	git -C $_dir checkout origin/$_branch
-	git -C $_dir reset --hard
-	git -C $_dir pull origin $_branch
 
+	else
+		git -C $_dir remote -v | grep 'git@' >/dev/null
+		if [ $? -ne 0 ]; then
+			git -C $_dir fetch --all
+			git -C $_dir pull origin $_branch
+		fi
+	fi
 	if [ -f "$_dir/scripts/run" ]; then
 		echo "========================="
 		echo "$_dir/scripts/run _prepare"
@@ -46,29 +50,24 @@ _update_sources() {
 		_url=$(echo $_pathgit | cut -d'|' -f2)
 		_branch=$(echo $_pathgit | cut -d'|' -f3)
 		if [ -z "$_branch" ]; then _branch=$branch; fi
-		if [ ! -d "$_path" ]; then
+		if [ ! -d "$_path/.git" ]; then
 			git clone $_url $_path -b $_branch
-		fi
-
-		if [ ! -d "$_path" ]; then continue; fi
-
-		git -C $_path fetch --all
-
-		git -C $_path checkout $_branch
-		git -C $_path reset --hard
-		tmp="$(git -C $_path pull 2>&1)"
-		# echo "$tmp"
-		# echo "$tmp" | grep -i "error"
-		# if [ $? -eq 0 ]; then
-		# 	timeout 60 git -C $_path reset --hard
-		# 	tmp="$(timeout 60 git -C $_path pull origin $_branch 2>&1)"
-		# fi
-
-		echo "$tmp" | grep -i "updating"
-		st=$?
-		echo $_path $st
-		if [ $st -eq 0 ]; then
+			git -C $_path fetch --all
+			git -C $_path branch --set-upstream-to=origin/$_branch
 			_is_reload=1
+		else
+
+			git -C $_path fetch --all
+			git -C $_path checkout $_branch
+			tmp="$(git -C $_path pull 2>&1)"
+
+			echo "$tmp" | grep -i "updating"
+			st=$?
+			echo $_path $st
+			if [ $st -eq 0 ]; then
+				_is_reload=1
+			fi
+
 		fi
 
 	done
