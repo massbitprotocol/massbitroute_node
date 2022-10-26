7637bbcb4ebf90dfe21fad2f4f7386987e63c653
Vu Tran <baysao@gmail.com>
2022-10-26 14:26:24 +0700

    chore: add debug after success install

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -4,10 +4,10 @@ SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
 export HOME=$SITE_ROOT
 cd $SITE_ROOT
 cmd=$SITE_ROOT/cmd_server
-source $SITE_ROOT/.env_raw
-# source $SITE_ROOT/scripts/base.sh
-# _load_env $SITE_ROOT
+source $SITE_ROOT/env/env.sh
+
 debug_log=$SITE_ROOT/logs/debug.log
+
 # _send_log() {
 
 # 	id=$(cat $SITE_ROOT/vars/ID)
@@ -29,11 +29,6 @@ debug_log=$SITE_ROOT/logs/debug.log
 # truncate -s 0 $log_dir/nginx/*.log
 id=$(cat $SITE_ROOT/vars/ID)
 
-#$log_dir/stat-* $log_dir/nginx-* $log_dir/monitor_client*
-# for f in $log_dir/stat--* /etc/supervisor/conf.d/*--* /etc/supervisor/conf.d/mbr_*.conf; do
-# 	rm $f
-# done
-
 echo "$(TZ=":Asia/Ho_Chi_Minh" date)" >$debug_log
 
 echo "--OS" >>$debug_log
@@ -41,24 +36,8 @@ cat /etc/lsb-release >>$debug_log
 mid=$(cat /etc/machine-id)
 echo "machine_id:$mid" >>$debug_log
 
-# echo "--Git" >>$debug_log
-# for d in $SITE_ROOT $SITE_ROOT/etc/mkagent /massbit/massbitroute/app/gbc /etc/letsencrypt; do
-# 	echo "git dir:$d" >>$debug_log
-# 	git -C $d remote -v >>$debug_log
-# 	git -C $d pull >>$debug_log
-# done
 echo "----Vars" >>$debug_log
 find $SITE_ROOT/vars -type f | while read f; do echo $f $(cat $f) >>$debug_log; done
-
-# echo "----Debug script" >>$debug_log
-# _sc_debug=$SITE_ROOT/scripts/debug/$mid
-# echo "Script debug:$_sc_debug" >>$debug_log
-# if [ -f " $_sc_debug" ]; then
-# 	echo "Found patch: $_sc_debug" >>$debug_log
-# 	cat $_sc_debug >>$debug_log
-# 	echo "Run output" >>$debug_log
-# 	bash $_sc_debug 2>&1 >>$debug_log
-# fi
 
 echo "----ENV" >>$debug_log
 cat $SITE_ROOT/env/env.sh >>$debug_log
@@ -81,9 +60,6 @@ echo "--Network interface" >>$debug_log
 ifconfig >>$debug_log
 echo "--Netstat" >>$debug_log
 netstat -tunalp | grep -i listen >>$debug_log
-# echo "--Verify" >>$debug_log
-#$mbr $type register >>$debug_log
-# $mbr $type nodeverify >>$debug_log
 
 echo "--Docker" >>$debug_log
 docker ps >>$debug_log
@@ -96,13 +72,4 @@ lsof -p $(cat $SITE_ROOT/tmp/nginx.pid) >>$debug_log
 
 echo "----Nginx" >>$debug_log
 $cmd nginx -t 2>&1 | tee -a $debug_log
-# $cmd nginx -s reload 2>&1 | tee -a $debug_log
 $cmd nginx -T | tee -a $debug_log
-# >$nginx_error
-
-# _sc=$SITE_ROOT/${TYPE}deploy/debug/${mid}.sh
-# if [ -f "$_sc" ]; then
-# 	bash $_sc $SITE_ROOT | tee -a $debug_log
-# fi
-
-#_send_log
